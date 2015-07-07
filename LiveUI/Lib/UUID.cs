ce36e690b99c0aef628bcd959017e98c9b8a81c5
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2015-07-07 23:49:49 +0100

    all builds with new nugets

---
+++ LiveUI/Lib/UUID.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveUI.Lib
+{
+    class UUID
+    {
+
+        public static string toString()
+        {
+            string UUID = Refractored.Xam.Settings.CrossSettings.Current.GetValueOrDefault("LiveUI.UUID", "");
+            if (UUID == null || UUID.Length == 0)
+            {
+                UUID = Guid.NewGuid().ToString("D");
+                Refractored.Xam.Settings.CrossSettings.Current.AddOrUpdateValue("LiveUI.UUID", UUID);
+            }
+            return UUID;
+        }
+
+    }
+}
--- LiveUI/Networking/Headers.cs
+++ LiveUI/Networking/Headers.cs
@@ -5,7 +5,6 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using Path.
 
 namespace LiveUI.Networking
 {
@@ -17,9 +16,23 @@ namespace LiveUI.Networking
             client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
 
             // Device
-            client.DefaultRequestHeaders.Add("", "");
+            client.DefaultRequestHeaders.Add("X-Platform", DeviceInfo.Plugin.CrossDeviceInfo.Current.Platform.ToString());
+            client.DefaultRequestHeaders.Add("X-DeviceModel", DeviceInfo.Plugin.CrossDeviceInfo.Current.Model.ToString());
+            client.DefaultRequestHeaders.Add("X-OsVersion", DeviceInfo.Plugin.CrossDeviceInfo.Current.Version.ToString());
 
-            // OS
+            // API
+            client.DefaultRequestHeaders.Add("X-ApiVersion", "1.0");
+            
+            // Environment
+            client.DefaultRequestHeaders.Add("X-AppUUID", LiveUI.Lib.UUID.toString());
+            client.DefaultRequestHeaders.Add("X-AppVersion", Version.Plugin.CrossVersion.Current.Version);
+
+            client.DefaultRequestHeaders.Add("X-DeviceBatteryStatus", Battery.Plugin.CrossBattery.Current.Status.ToString());
+            client.DefaultRequestHeaders.Add("X-DeviceBatteryLevel", Battery.Plugin.CrossBattery.Current.RemainingChargePercent.ToString());
+            //client.DefaultRequestHeaders.Add("X-XXXXXXXX", Connectivity.Plugin.CrossConnectivity.Current.);
+            client.DefaultRequestHeaders.Add("X-XXXXXXXX", Version.Plugin.CrossVersion.Current.Version);
+            client.DefaultRequestHeaders.Add("X-XXXXXXXX", Version.Plugin.CrossVersion.Current.Version);
+            client.DefaultRequestHeaders.Add("X-XXXXXXXX", Version.Plugin.CrossVersion.Current.Version);
         }
 
     }
