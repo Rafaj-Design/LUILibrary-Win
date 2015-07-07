59ce5c8e79563c6699280e92e32f4bbb437f29f5
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2015-07-07 13:07:49 +0100

    initial commit

---
+++ LiveUI/Main.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LiveUI
+{
+    public class Main
+    {
+        public string apiKey { get; set; }
+    }
+}
