dd87e5c8259706b0408854252947448e60fe43eb
Ondrej Rafaj <ondrej.rafaj@gmail.com>
2015-07-08 00:14:34 +0100

    checking for key before updating

--- LiveUI/Main.cs
+++ LiveUI/Main.cs
@@ -8,11 +8,20 @@ namespace LiveUI
 {
     public class Main
     {
-        public string apiKey { get; set; }
+
+        public string apiKey { get; set; } = null;
 
         public void update()
         {
-            Translations.Translations.Instance.update();
+            if (this.apiKey != null && this.apiKey.Length > 0)
+            {
+                Translations.Translations.Instance.update();
+            }
+            else
+            {
+                throw new Exception("LiveUI API key has not been set!");
+            }
         }
+
     }
 }
